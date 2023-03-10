5a75fc5ac09c73ca486cca7d3b33e512cf8ec7ed
Ted Bataller <ted.jerbybataller@gmail.com>
2023-03-10 17:28:45 +0800

    Added new project

--- media_file_module/scripts/static.js
+++ media_file_module/scripts/static.js
@@ -6,30 +6,16 @@ let mimes = {
     "mp3": {path:"../media/", type:"audio/mpg"},
     "jpg": {path:"../images/", type:"image/jpeg"},
     "mp4": {path:"../media/", type:"video/mp4"},
-    "ico": {path:"../images/", type:"image/vnd.microsoft.icon"}
+    "ico": {path:"../images/", type:"image/vnd.microsoft.icon"},
+    "png": {path:"../images/", type:"image/png"}
 }
-function getExt(request)
-{
-    let urlArr = [];
-    let extension = "";
-    for(let x=request.url.length; x>0; x--)
-    { 
-        if(request.url[x-1] == "."){break;}
-        urlArr[urlArr.length] = request.url[x - 1];
-    }
-    for(let i=urlArr.length-1; i>=0; i--)
-    {
-        extension += urlArr[i];
-    }
-    return extension;
-}
-function getFile(request)
+function getFile(request, r)
 {
     let fileArr = [];
     let fileName = "";
     for(let x=request.url.length; x>0; x--)
     { 
-        if(request.url[x-1] == "/"){break;}
+        if(request.url[x-1] == r){break;}
         fileArr[fileArr.length] = request.url[x - 1];
     }
     for(let i=fileArr.length-1; i>=0; i--)
@@ -39,40 +25,53 @@ function getFile(request)
     return fileName;
 }
 module.exports = function(request, response){
-    let fileExt = "";
-    let filePath = "";
-    let fileType = "";
-    let fileFolder = "";
+
+    let fileExt = getFile(request, ".");
+    let mime = mimes[fileExt] || "";
+    let fileFolder = mime.path || "";
+    let filePath = fileFolder+getFile(request, "/");
+    let fileType = mime.type || "text/html";
     if(request.url === "/"){
         filePath = "../views/index.html";
         fileType = "text/html";
     }
-    else
-    {
-        fileExt = getExt(request);
-        fileFolder = mimes[fileExt].path;
-        filePath = fileFolder+getFile(request);
-        fileType = mimes[fileExt].type;
-        
-    }
     fs.stat(filePath, function(err, stat)
     {
         if (err == null || request.url === "/") {
-            fs.readFile(filePath,'utf8',function(errors, contents)
+            console.log("exists");
+            if(fileExt === "png" || fileExt === "jpg" || fileExt === "ico")
             {
-                response.writeHead(200, {'Content-Type': fileType});
+                fs.readFile(filePath,function(errors, contents)
+                {
+                    if(!errors)
+                    {
+                        response.writeHead(200, {'Content-Type': fileType});
+                        response.write(contents);
+                        response.end();
+                    }
+                })
+            }
+            else
+            {
+                fs.readFile(filePath,'utf8',function(errors, contents)
+                {
+                    if(!errors)
+                    {
+                        response.writeHead(200, {'Content-Type': fileType});
+                        response.write(contents);
+                        response.end();
+                    }
+                })
+            }
+
+        }
+        else
+        {
+            fs.readFile('../views/error.html', function(errors, contents){
+                response.writeHead(200, {'Content-Type': 'text/html'});
                 response.write(contents);
                 response.end();
             })
-        } else if (err.code === 'ENOENT') {
-            fs.readFile('views/error.html','utf8',function(errors, contents)
-            {
-                response.writeHead(200, {'Content-Type': "text/html"});
-                response.write(contents);
-                response.end();
-            })
-        } else {
-            console.log('Some other error: ', err.code);
         }
     })
 }
