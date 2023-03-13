2e871dbb1cb4e1450b9cae450e3f16f697e12007
Ted Bataller <ted.jerbybataller@gmail.com>
2023-03-13 23:31:41 +0800

    Added new activities

--- express_sample/app.js
+++ express_sample/app.js
@@ -1,23 +1,12 @@
-(function(){
-    let express = require("express");
-    let app = express();
-    // console.log(app.get());
+let express = require("express");
+let app = express();
+let port = 8000;
 
-    app.set("views",__dirname + "/views");
-    app.set("view engine","ejs");
-
-    app.get("/justme",function(request, response)
-    {
-        var users_array = [
-            {name: "Michael", email: "michael@codingdojo.com"},
-            {name: "Jay", email: "jay@codingdojo.com"}, 
-            {name: "Brendan", email: "brendan@codingdojo.com"}, 
-            {name: "Andrew", email: "andrew@codingdojo.com"}
-        ];
-        response.render('users', {users: users_array});
-    })
-
-    app.listen(8000, function(){
-        console.log("listening to port 8000");
-    })
-}())
+app.get("/", function(req, res)
+{
+    res.render();
+})
+app.listen(port, function()
+{
+    console.log("Listening to port "+port);
+})
